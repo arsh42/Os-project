cedf86a7083c422b6c4cf7cf763186822cc7cc5b
Harsh Sharma <31186559+arsh42@users.noreply.github.com>
2019-04-07 08:24:31 +0100

    Add files via upload

---
+++ page.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float nm,acm,eat,r,f,m;
+    printf(" ---------Page Vaule For Fault Service time Not modified-------- : ");
+    scanf("%f",&nm);
+    printf("------Page vaule For Fault Service Time Modified------ : ");
+    scanf("%f",&m);
+    printf("Enter Access Time Of Memory :  ");
+    scanf("%f",&acm);
+   
