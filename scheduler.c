2b612690579e462a8573c129f9bc3f995aad8e4c
Harsh Sharma <31186559+arsh42@users.noreply.github.com>
2019-04-07 08:43:05 +0100

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -7,19 +7,19 @@ int main()
 {
 int At[10],Bt[10],Rt[10],EndTime,i,Shortest;
 int Remain=0,n,Time,Sum_Wait=0,Sum_Turnaround=0;
-printf("-----------------Welcome To The Scheduler--------------\n");
-printf("Enter The Process details \n\n");
-printf("No Of Processes : ");
+
+
+printf("enter number of processes: ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {	
-printf("<<<<<<<<<<<-Arrival Times of Processes P%d ->>>>>>>>>>>> ",i+1);
+printf("arrival time of processes P%d  ",i+1);
 scanf("%d",&At[i]);
-printf("<<<<<<<<<<<<- Burst Times of Processes P%d ->>>>>>>>>>> ",i+1);
+printf("burst time of processes P%d ",i+1);
 scanf("%d",&Bt[i]);
 Rt[i]=Bt[i];
 }
-printf("\n\nProcess\t|Turnaround Time| Waiting Time\n\n");
+printf("\n\nprocess\t|turnaround_time| waiting_time\n");
 Rt[9]=9999;
 for(Time=0;Remain!=n;Time++)
 {
